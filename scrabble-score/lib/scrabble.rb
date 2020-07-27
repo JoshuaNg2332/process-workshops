48d20638fb86e6632ca00b83e5a2530474baf056
Josh <joshua.ng2332@gmail.com>
2020-07-27 17:09:23 +0100

    starts the game with a score of 0.

--- scrabble-score/lib/scrabble.rb
+++ scrabble-score/lib/scrabble.rb
@@ -1,9 +1,12 @@
 class Scrabble
 
   def user_input(user_input)
-
     @user_input = user_input
-    
   end
+
+  def score
+    score = 0
+  end
+
 
 end
--- scrabble-score/spec/scrabble_spec.rb
+++ scrabble-score/spec/scrabble_spec.rb
@@ -2,4 +2,7 @@ describe Scrabble do
   it 'gets the user input and outputs a word' do
     expect(subject.user_input("hello")).to eq("hello")
   end
+  it 'starts the game with a score of 0' do
+    expect(subject.score).to eq(0)
+  end
 end
